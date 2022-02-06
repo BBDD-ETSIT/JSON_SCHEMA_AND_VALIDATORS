e778f01a9b04d4d43014614b646f0a4a649cfd56
Enrique <enrique.barra@upm.es>
2022-02-06 22:19:42 +0100

    name change

--- validator.js
+++ validator.js
@@ -1,23 +1,17 @@
 // Node.js require:
-const Ajv = require("ajv")
+const Ajv = require("ajv/dist/2020");
+const addFormats = require("ajv-formats");
 
 const ajv = new Ajv() // options can be passed, e.g. {allErrors: true}
+addFormats(ajv);
 
-const schema = {
-  type: "object",
-  properties: {
-    foo: {type: "integer"},
-    bar: {type: "string"}
-  },
-  required: ["foo"],
-  additionalProperties: false,
+const schema = require('./catalogo.schema.json');
+const data = require('./catalogo.json');
+
+const validate = ajv.compile(schema);
+const valid = validate(data);
+if (valid) {
+  console.log("JSON VALIDO!");
+} else {
+  console.log(validate.errors);
 }
-
-const data = {
-  foo: "1",
-  bar: "abc"
-}
-
-const validate = ajv.compile(schema)
-const valid = validate(data)
-if (!valid) console.log(validate.errors)
